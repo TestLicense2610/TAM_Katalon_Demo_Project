91ac8de6a45b87ce1aa326de4d3d3934ea5ea5a1
linh.nguyen <linh.nguyen@localhost>
2025-08-17 21:49:46 +0700

    Test Suite

---
+++ Test Suites/TrueTest/HelpDesk_TS.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>HelpDesk_TS</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>1ec36783-f012-45fc-acca-bd63a9869137</testSuiteGuid>
+</TestSuiteEntity>
