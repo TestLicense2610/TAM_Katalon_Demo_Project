8ce12541c107f0bc6c286a686eaf712966d8d219
linh.nguyen <linh.nguyen@localhost>
2025-08-21 08:37:15 +0700

    commit 3

---
+++ Test Suites/WebUI/WebUI_TS.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>WebUI_TS</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>10</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>c142ddd5-cfa7-4b96-84fe-5329028a1ceb</testSuiteGuid>
+   <testCaseLink>
+      <guid>7cd96df5-8707-49eb-a3b9-44a45d6b859b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/WebUI/CuraHealth_TC</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
